b96337a5853ce6b45ceb37de60756ec89bd3855f
Daniel Gil <dannyjaygil@gmail.com>
2024-03-01 16:29:52 +0200

    Save

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -11,17 +11,17 @@ async function Page({
     maxPrice?: string;
   };
 }) {
-  // const properties = await fetch(
-  //   `https://webapi.resales-online.com/V6/SearchProperties?p_agency_filterid=1&p1=1018186&p2=a6f757f38647f9fed6a748b29c64012a242cae1b&P_sandbox=true&P_Location=${searchParams?.area}&P_PropertyTypes=${searchParams?.propertyType}&P_Beds=${searchParams?.bedrooms}&P_Min=${searchParams?.minPrice}&P_Max=${searchParams?.maxPrice}`
-  // )
-  //   .then((res) => res.json())
-  //   .then((data) => data as any);
-
   const properties = await fetch(
-    `https://webapi.resales-online.com/V6/SearchProperties?p_agency_filterid=1&p1=1018186&p2= 057d095bc4e7c27c3de591fbd536f405f091bbeb&P_sandbox=true&P_Location=${searchParams?.area}&P_PropertyTypes=${searchParams?.propertyType}&P_Beds=${searchParams?.bedrooms}&P_Min=${searchParams?.minPrice}&P_Max=${searchParams?.maxPrice}`
+    `https://webapi.resales-online.com/V6/SearchProperties?p_agency_filterid=1&p1=1018186&p2=a6f757f38647f9fed6a748b29c64012a242cae1b&P_sandbox=true&P_Location=${searchParams?.area}&P_PropertyTypes=${searchParams?.propertyType}&P_Beds=${searchParams?.bedrooms}&P_Min=${searchParams?.minPrice}&P_Max=${searchParams?.maxPrice}`
   )
     .then((res) => res.json())
     .then((data) => data as any);
+
+  // const properties = await fetch(
+  //   `https://webapi.resales-online.com/V6/SearchProperties?p_agency_filterid=1&p1=1018186&p2= 057d095bc4e7c27c3de591fbd536f405f091bbeb&P_sandbox=true&P_Location=${searchParams?.area}&P_PropertyTypes=${searchParams?.propertyType}&P_Beds=${searchParams?.bedrooms}&P_Min=${searchParams?.minPrice}&P_Max=${searchParams?.maxPrice}`
+  // )
+  //   .then((res) => res.json())
+  //   .then((data) => data as any);
 
   return (
     <main className="mt-10">
